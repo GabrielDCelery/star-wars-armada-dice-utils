855d60599ec0be7d17c3a431093c75959bd87b61
Gabriel Zeller <gabriel.d.celery@gmail.com>
2020-02-29 19:56:00 +0000

    Added dist folder to gitignore

--- dist/common/enums/EArmadaDice.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EArmadaDice;
-(function (EArmadaDice) {
-    EArmadaDice["RED_DICE"] = "red";
-    EArmadaDice["BLUE_DICE"] = "blue";
-    EArmadaDice["BLACK_DICE"] = "black";
-})(EArmadaDice = exports.EArmadaDice || (exports.EArmadaDice = {}));
